8b0cfc56256d53ec93a784afa9b9ab25bd8bd139
PhamTrieu0703 <thanhtrieupham10@gmail.com>
2024-03-27 18:13:56 +0700

    push local project to Git repo

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>0bda7356-84b5-4fa5-bc25-14d9f2b48612</testSuiteGuid>
+   <testCaseLink>
+      <guid>fff422b9-d5da-48c3-88e0-9cabc698863f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>f5260817-02f8-4d6a-b9b8-32cf1e8b0d4f</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/New Test Data</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>f5260817-02f8-4d6a-b9b8-32cf1e8b0d4f</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>6ba67ea3-041e-4979-aeb3-08290ac89927</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>f5260817-02f8-4d6a-b9b8-32cf1e8b0d4f</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>a7d4e3fa-9d50-4ded-84c1-408c3e328c8c</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
